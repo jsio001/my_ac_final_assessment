a28f52fb186384f7d3180e8b465166b0e36deaf9
jsio001 <jsio001@e.ntu.edu.sg>
2017-11-29 13:36:18 +0800

    seeds with user_note assoc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-unless Note.count > 10
-  10.times do
-    Note.create(title: "Note No."+ Faker::Number.between(1, 10).to_s, body: Faker::Hipster.paragraph(2, false, 4))
+unless User.count > 5
+  User.create(email: 'user1@mail.com', password: '123456', password_confirmation: '123456')
+  User.create(email: 'user2@mail.com', password: '123456', password_confirmation: '123456')
+  User.create(email: 'user3@mail.com', password: '123456', password_confirmation: '123456')
+  User.create(email: 'user4@mail.com', password: '123456', password_confirmation: '123456')
+  User.create(email: 'user5@mail.com', password: '123456', password_confirmation: '123456')
+  p "users created"
+end
+
+unless Note.count > 20
+  5.times do
+    Note.create(title: "Note No."+ Faker::Number.between(1, 40).to_s, body: Faker::Hipster.paragraph(2, false, 4), user_id: 1)
+  end
+  5.times do
+    Note.create(title: "Note No."+ Faker::Number.between(1, 40).to_s, body: Faker::Hipster.paragraph(2, false, 4), user_id: 2)
+  end
+  5.times do
+    Note.create(title: "Note No."+ Faker::Number.between(1, 40).to_s, body: Faker::Hipster.paragraph(2, false, 4), user_id: 3)
+  end
+  5.times do
+    Note.create(title: "Note No."+ Faker::Number.between(1, 40).to_s, body: Faker::Hipster.paragraph(2, false, 4), user_id: 4)
   end
   p "notes created"
 end
